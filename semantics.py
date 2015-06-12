0d54ff8fa62b5e407e2b18b209b911abf169c6af
Sun Xiaofei <adoni1203@gmail.com>
2015-06-12 10:35:00 +0800

    add templates

--- semantics.py
+++ semantics.py
@@ -1,6 +1,8 @@
+import web
+render = web.template.render('templates/')
 class semantics_index:
     def GET(self):
-        return 'Semantics'
+        return render.semantics_index()
 
 class semantics_show:
     def GET(self):
