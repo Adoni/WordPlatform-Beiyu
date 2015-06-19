ee51ba13c331b8fe3527268f3e33c1ea0250aad2
Sun Xiaofei <adoni1203@gmail.com>
2015-06-19 15:24:10 +0800

    edit html templates

--- semantics.py
+++ semantics.py
@@ -1,9 +1,24 @@
 import web
 render = web.template.render('templates/',base='layout')
+from word_handler import word_handler
+handler=word_handler()
+
 class semantics_index:
     def GET(self):
-        return render.semantics_index()
+        dates=[2005,2006,2007,2008]
+        return render.semantics_index(dates=dates)
 
 class semantics_show:
     def GET(self):
         return 'Semantics show'
+    def POST(self):
+        params = web.input()
+        word=params['word']
+        dates=[]
+        for p in params:
+            if params[p]=='on':
+                dates.append(p)
+        words=handler.get_closest_words(dates,word)
+        words=zip(dates,words)
+        dates=[2005,2006,2007,2008]
+        return render.semantics_index(dates=dates,words=words)
