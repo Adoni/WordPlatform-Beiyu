7bf7cd8c1bb75d097002d9396d66aa6476bce82d
root <root@OpenAPI.cloud.mos.meituan.local>
2015-07-10 12:59:53 +0800

    start to add echart

--- helper.py
+++ helper.py
@@ -11,7 +11,7 @@ def distant(a,b):
     return dist
 
 def get_word_meaning(date,word):
-    dates=range(2005,date+1)
+    dates=range(2005,int(date)+1)
     list_words=deliver.get_closest_words(dates,word)
     closest_words=[]
     for l in list_words:
@@ -36,15 +36,18 @@ def get_word_meaning(date,word):
 
 def get_distance(date,word,meaning):
     word_embedding=deliver.get_word_embedding(date,word)
-    for m in meaning:
-        print ' '.join(map(lambda d:d[0].encode('utf8'),m['words']))
-        print distant(word_embedding,m['center'])
+    dist=map(lambda m:distant(word_embedding,m['center']),meaning)
+    return dist
 
+def get_batch_distant(dates,word):
+    batch_distant=dict()
+    for date in dates:
+        meaning=get_word_meaning(date,word)
+        dist=get_distance(date,word,meaning)
+        batch_distant[date]=zip(map(lambda m:map(lambda mm:mm[0],m['words']),meaning),dist)
+    return batch_distant
 
 if __name__=='__main__':
     word=u'小米'
     word=u'第三者'
-    for year in range(2005,2012):
-        print year
-        meaning=get_word_meaning(year,word)
-        get_distance(year,word,meaning)
+    print get_batch_distant([2005,2010],word)
--- semantics.py
+++ semantics.py
@@ -1,6 +1,7 @@
 import web
 render = web.template.render('templates/',base='layout')
 from deliver import Deliver
+from helper import get_batch_distant
 
 class semantics_index:
     def GET(self):
@@ -19,10 +20,17 @@ class semantics_show:
                 checked_dates.append(p)
         checked_dates=map(lambda d:int(d),checked_dates)
         checked_dates=sorted(checked_dates)
-        dates=map(lambda d:str(d),checked_dates)
+        checked_dates=map(lambda d:str(d),checked_dates)
         deliver=Deliver()
-        words=deliver.get_closest_words(dates,word)
-        print words
-        words=zip(dates,words)
+        words=deliver.get_closest_words(checked_dates,word)
+        batch_distant=get_batch_distant(checked_dates,word)
+        words=dict(zip(checked_dates,words))
         dates=[2005,2006,2007,2008,2009,2010,2011,2012]
-        return render.semantics_show(dates=dates,word=word,words=words,checked_dates=checked_dates)
+        dates=map(lambda date:str(date),dates)
+        return render.semantics_show(
+                dates=dates,
+                checked_dates=checked_dates,
+                word=word,
+                words=words,
+                batch_distant=batch_distant,
+                )
