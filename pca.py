b8a54b9a0460c3355422a4c7daa66a372c700a11
root <root@OpenAPI.cloud.mos.meituan.local>
2015-10-09 13:43:52 +0800

    try to use echarts to plot pca

--- pca.py
+++ pca.py
@@ -1,42 +1,96 @@
-import numpy as np
-from small_utils.vector_reader import read_vectors
-from small_utils.vector_reader import simple_embedding_cluster_viewer
-from sklearn.decomposition import PCA
+#coding:utf8
+import web
+from deliver import Deliver
+from helper import get_batch_distant
+from helper import transfer_to_json
+from helper import transfer_to_line
+import json
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib.pyplot import figure
-from matplotlib.patches import Ellipse
+from matplotlib.font_manager import FontProperties
 import mpld3
+from small_utils.vector_reader import read_vectors
+from settings import Embedding_Dir
+import StringIO
+import base64
+from matplotlib import colors
+import six
 
-base_dir='/Users/sunxiaofei/Desktop/'
-def draw(data,labels,ax):
+render = web.template.render('templates/',base='layout')
+
+'''
+deliver是用于和提供相近词模块的交互的中间层
+'''
+deliver=Deliver()
+embedding=read_vectors('%s/pca_embedding.data'%Embedding_Dir,as_dict=True)
+colors_=list(six.iteritems(colors.cnames))
+
+'''
+available_dates指当前可以获取的年份信息
+'''
+available_dates=[2005,2006,2007,2008,2009,2010,2011,2012,2013]
+available_dates=map(lambda d:str(d),available_dates)
+
+def draw(data,labels,ax,color):
     x=map(lambda d:d[0],data)
     y=map(lambda d:d[1],data)
-    line, = ax.plot(x, y, '.-', color='purple')
+    line, = ax.plot(x, y, '.-', color=color)
+    font = FontProperties(fname=r"/usr/share/fonts/truetype/wqy/wqy-microhei.ttc", size=10)
+    print color
     for l in labels:
         ax.annotate(l.decode('utf8'),
                 xy=labels[l],
+                fontproperties=font,
                 )
 
-def pca_plot(words,years,embedding):
+def pca_plot(words,years):
     fig = figure()
     ax = fig.add_subplot(111)
-    for w in words:
+    for index,w in enumerate(words):
+        w=w.encode('utf8')
         all_embedding=[]
         all_labels=dict()
         for year in years:
             pca_w=str(year)+w
             if pca_w in embedding:
-                all_embedding.append(embedding[pca_w])
-                all_labels[pca_w]=embedding[pca_w]
+                xy=embedding[pca_w]
+                all_embedding.append(xy)
+                all_labels[pca_w]=xy
             else:
-                print 'Not in year %d'%year
-        draw(all_embedding,all_labels,ax)
-    print mpld3.fig_to_html(fig)
+                print 'Not in year %s'%str(year)
+        draw(all_embedding,all_labels,ax,color=colors_[index][0])
+    buf=StringIO.StringIO()
+    fig.savefig(buf, format='png')
+    return base64.encodestring(buf.getvalue())
 
-def test():
-    embedding=read_vectors(base_dir+'pca_embedding.data',as_dict=True)
-    while 1:
-        words=raw_input('Words:').strip().split(' ')
-        pca_plot(words,[2005,2006],embedding)
+class pca_index:
+    def GET(self):
+        return render.pca_index(dates=available_dates)
+
+class pca_show:
+    def GET(self):
+        raise web.seeother('/pca')
+    def POST(self):
+        params = web.input()
+        words=params['words'].strip().split(' ')
+        checked_dates=[]
+        for p in available_dates:
+            if p in params and params[p]=='on':
+                checked_dates.append(p)
+        checked_dates=map(lambda d:int(d),checked_dates)
+        checked_dates=sorted(checked_dates)
+        checked_dates=map(lambda d:str(d),checked_dates)
+        for word in words:
+            checked_dates=filter(lambda d:not deliver.get_word_embedding(d,word) is None,checked_dates)
+        if checked_dates==[]:
+            return render.error(info="Cannot find those in our corpus!")
+        fig=pca_plot(words,checked_dates)
+        return render.pca_show(
+                dates=available_dates,
+                checked_dates=checked_dates,
+                fig=fig
+                )
 
 if __name__=='__main__':
-    test()
+    print pca_plot(['中国'],[2005,2006,2007])
